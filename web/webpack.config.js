281e3ccb8e53f42f5286c6fa42081417ade5bf03
Takashi Ando <dodo5522@gmail.com>
2018-10-20 07:26:57 +0900

    env: polyfill js code

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/geiger-chart.js',
+	mode: 'development',
+	entry: ['@babel/polyfill', './src/geiger-chart.js'],
 	output: {
 		filename: 'geiger-chart.js',
 		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env',
+						]
+					}
+				}
+			},
+		]
 	},
 };
